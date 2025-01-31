33b5b56c11b93bc64b58ef728084b5378a5b170e
t-hanna <97122221+t-hanna@users.noreply.github.com>
2025-01-31 12:17:31 -0500

    changes to Drawer without > icon

--- vivra_bottle_app/src/components/drawer/Drawer.tsx
+++ vivra_bottle_app/src/components/drawer/Drawer.tsx
@@ -1,94 +1,22 @@
 "use client";
 import { FC } from "react";
 
-import {
-  ActionIcon,
-  Drawer as MantineDrawer,
-  NavLink,
-  ThemeIcon,
-  useComputedColorScheme,
-  useMantineColorScheme,
-} from "@mantine/core";
-
-import classes from "./Drawer.module.css";
+import { Drawer as MantineDrawer, NavLink } from "@mantine/core";
 
 export interface HeaderProps {
   opened: boolean;
   close: () => void;
 }
-
-// TODO make the rest of the pages and add the navlinks and icons then test
-// TODO fix loading page
+// TODO, delete Drawer.module.css if not being used
+// Does it look weird on the page with the proportions? Add icon or something?
+// Fix color scheme why does it break? Maybe cause 404 ERROR?
 export const Drawer: FC<HeaderProps> = ({ opened, close }) => {
-  const { setColorScheme } = useMantineColorScheme();
-
-  const colorMode = useComputedColorScheme("light");
   return (
     <MantineDrawer opened={opened} onClose={close} title="Options">
-      <NavLink
-        href="/"
-        label="About us"
-        rightSection={
-          <ThemeIcon
-            style={{
-              backgroundColor: "transparent",
-            }}
-          >
-            <img
-              className={classes.chevron}
-              style={{
-                backgroundColor: "transparent",
-                height: "12px",
-                width: "12px",
-              }}
-              src={
-                colorMode == "dark"
-                  ? "/assets/icons/chevron-right-solid-dark.svg"
-                  : "/assets/icons/chevron-right-solid-light.svg"
-              }
-            ></img>
-          </ThemeIcon>
-        }
-      />
-      <NavLink
-        href="/my-profile"
-        label="Summary"
-        rightSection={
-          <ThemeIcon
-            style={{
-              backgroundColor: "transparent",
-            }}
-          >
-            <img
-              className={classes.chevron}
-              style={{
-                backgroundColor: "transparent",
-                height: "12px",
-                width: "12px",
-              }}
-              src={
-                colorMode == "dark"
-                  ? "/assets/icons/chevron-right-solid-dark.svg"
-                  : "/assets/icons/chevron-right-solid-light.svg"
-              }
-            ></img>
-          </ThemeIcon>
-        }
-      />
-
-      <NavLink href="/" label="History" />
-      <NavLink
-        href="/profile-settings"
-        label="Profile Settings"
-        // leftSection={<IconActivity size={16} stroke={1.5} />}
-        // rightSection={
-        //   <IconChevronRight
-        //     size={12}
-        //     stroke={1.5}
-        //     className="mantine-rotate-rtl"
-        //   />
-        // }
-      />
+      <NavLink href="/my-profile" label="Summary" />
+      <NavLink href="/my-history" label="History" />
+      <NavLink href="/about-us" label="About us" />
+      <NavLink href="/settings" label="Settings" />
     </MantineDrawer>
   );
 };
