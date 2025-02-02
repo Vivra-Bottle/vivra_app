96b62b2bbbea5cc17dbb22bb5a5800ad0959e130
t-hanna <97122221+t-hanna@users.noreply.github.com>
2025-02-02 16:30:05 -0500

    add css styling

--- vivra_bottle_app/src/app/loading.tsx
+++ vivra_bottle_app/src/app/loading.tsx
@@ -1,9 +1,10 @@
 import { Box, Skeleton as MantineSkeleton } from "@mantine/core";
+import classes from "./page.module.css";
 
 //TODO work on loading page
 export default function Loading() {
   return (
-    <Box mr={"10%"} ml={"10%"}>
+    <Box className={classes.loading}>
       <MantineSkeleton mt={"80px"} height={50} circle mb="xl" />
       <MantineSkeleton height={8} radius="xl" />
       <MantineSkeleton height={8} mt={6} radius="xl" />
