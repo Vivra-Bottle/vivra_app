e291bb82748be7cca45362859f7c9afdb9a88b46
t-hanna <97122221+t-hanna@users.noreply.github.com>
2025-02-16 18:08:37 -0500

    redirecting to summary page

--- vivra_bottle_app/src/components/header/Header.tsx
+++ vivra_bottle_app/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FC } from "react";
+import Link from "next/link";
 
 import { useDisclosure } from "@mantine/hooks";
 import {
@@ -10,7 +11,7 @@ import {
   useMantineColorScheme,
 } from "@mantine/core";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import classes from "./Header.module.css";
 import { Drawer } from "../drawer/Drawer";
@@ -40,12 +41,13 @@ export const Header: FC = () => {
     }
   }
 
-  const bluetoothConnect = async () => {  
+  const bluetoothConnect = async () => {
     try {
       console.log("Requesting Bluetooth Device...");
-      bleDevice = await navigator.bluetooth.requestDevice({ //This shows and error but is only ran if SSR is done, so not a real issue
-          acceptAllDevices: true,
-          optionalServices: [SERVICE_UUID]
+      bleDevice = await navigator.bluetooth.requestDevice({
+        //This shows and error but is only ran if SSR is done, so not a real issue
+        acceptAllDevices: true,
+        optionalServices: [SERVICE_UUID],
       });
 
       console.log("Connecting to GATT Server...");
@@ -55,16 +57,20 @@ export const Header: FC = () => {
       bleService = await bleServer?.getPrimaryService(SERVICE_UUID);
 
       console.log("Getting Characteristic...");
-      bleCharacteristic = await bleService?.getCharacteristic(CHARACTERISTIC_UUID);
+      bleCharacteristic = await bleService?.getCharacteristic(
+        CHARACTERISTIC_UUID
+      );
 
       console.log("Connected! Waiting for data...");
 
       // Enable notifications
       await bleCharacteristic?.startNotifications();
-      bleCharacteristic?.addEventListener("characteristicvaluechanged", handleNotifications);
-
+      bleCharacteristic?.addEventListener(
+        "characteristicvaluechanged",
+        handleNotifications
+      );
     } catch (error) {
-        console.log("Error:", error);
+      console.log("Error:", error);
     }
   };
 
@@ -75,7 +81,6 @@ export const Header: FC = () => {
       setIsBluetoothSupported(false);
     }
   }, []);
-
 
   return (
     <header className={classes.header}>
@@ -88,43 +93,46 @@ export const Header: FC = () => {
         />
         <Drawer opened={opened} close={close}></Drawer>
         <Box className={classes.vivra_icon}>
-          <Image
-            key={colorMode}
-            priority
-            alt="vivra icon"
-            src={
-              colorMode == "dark"
-                ? "/assets/icons/vivra-dark.svg"
-                : "/assets/icons/vivra-light.svg"
-            }
-            width={56}
-            height={56}
-          ></Image>
+          <Link href="/" passHref legacyBehavior>
+            <Image
+              key={colorMode}
+              priority
+              alt="vivra icon"
+              src={
+                colorMode == "dark"
+                  ? "/assets/icons/vivra-dark.svg"
+                  : "/assets/icons/vivra-light.svg"
+              }
+              width={56}
+              height={56}
+            ></Image>
+          </Link>
         </Box>
         <div className={classes.icons}>
           <ActionIcon
-          className={classes.action_icon}
-          variant="outline"
-          color={colorMode == "dark" ? "yellow" : "dark.7"}
-          title="Color Scheme Toggle"
-          onClick={() => {if (isBluetoothSupported) {
-            bluetoothConnect();
-            } else {
-              console.log("Bluetooth not supported");
+            className={classes.action_icon}
+            variant="outline"
+            color={colorMode == "dark" ? "yellow" : "dark.7"}
+            title="Color Scheme Toggle"
+            onClick={() => {
+              if (isBluetoothSupported) {
+                bluetoothConnect();
+              } else {
+                console.log("Bluetooth not supported");
+              }
             }}
-          }
           >
-          <Image
-            alt={colorMode == "dark" ? "sun icon" : "moon icon"}
-            className={classes.color_icon}
-            src={
-              colorMode == "dark"
-                ? "/assets/icons/bluetooth-brands-solid.svg"
-                : "/assets/icons/bluetooth-brands-solid.svg"
-            }
-            height={18}
-            width={18}
-          />
+            <Image
+              alt={colorMode == "dark" ? "sun icon" : "moon icon"}
+              className={classes.color_icon}
+              src={
+                colorMode == "dark"
+                  ? "/assets/icons/bluetooth-brands-solid.svg"
+                  : "/assets/icons/bluetooth-brands-solid.svg"
+              }
+              height={18}
+              width={18}
+            />
           </ActionIcon>
           <ActionIcon
             className={classes.action_icon}
