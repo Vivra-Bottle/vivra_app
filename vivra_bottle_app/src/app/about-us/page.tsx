d4a9a6a6a1b6a1a940abbc5729a42cffe99c0072
t-hanna <97122221+t-hanna@users.noreply.github.com>
2025-02-16 23:21:56 -0500

    making water consumption card

--- vivra_bottle_app/src/app/about-us/page.tsx
+++ vivra_bottle_app/src/app/about-us/page.tsx
@@ -17,46 +17,44 @@ const vivraTimelineItems = [
 
 export default function AboutUs() {
   return (
-    <div>
-      <Stack display="Flex" mt={59} mb={59}>
-        <Image
-          alt="Water Bottle Picture"
-          src="/assets/pictures/water-bottle.jpg"
-        ></Image>
-        <Stack className={classes.about}>
-          <Title order={2}>About us</Title>
-          <Text>
-            Vivra was born from a mutual concern over water quality and
-            hydration levels in everyday life. We are a team of driven and
-            innovative 4th-year students from the University of Waterloo&apos;s
-            Faculty of Nanotechnology Engineering, collectively bringing eight
-            years of industry experience.
-          </Text>
-          <Text>
-            Our mission is to promote health and well-being through proper
-            hydration practices, addressing both water quality and consumption.
-            We are currently designing and fabricating a smart water bottle that
-            leverages advanced sensor technology—down to the nanoscale—to
-            monitor both water quality and consumption.
-          </Text>
-          <Text>
-            From the city with &quot;water&quot; in its name, we&apos;re leading
-            the way in smarter, safer hydration.
-          </Text>
-        </Stack>
-        <Stack className={classes.founders}>
-          <Title order={2}>Founders</Title>
-          <Image
-            radius="md"
-            alt="Team picture"
-            src="/assets/pictures/team-picture.jpg"
-          ></Image>
-        </Stack>
-        <Stack className={classes.story}>
-          <Title order={2}>Vivra&apos;s Story</Title>
-          <VivraTimeline active={1} items={vivraTimelineItems}></VivraTimeline>
-        </Stack>
+    <Stack display="Flex" mt={59} mb={59}>
+      <Image
+        alt="Water Bottle Picture"
+        src="/assets/pictures/water-bottle.jpg"
+      ></Image>
+      <Stack className={classes.about}>
+        <Title order={2}>About us</Title>
+        <Text>
+          Vivra was born from a mutual concern over water quality and hydration
+          levels in everyday life. We are a team of driven and innovative
+          4th-year students from the University of Waterloo&apos;s Faculty of
+          Nanotechnology Engineering, collectively bringing eight years of
+          industry experience.
+        </Text>
+        <Text>
+          Our mission is to promote health and well-being through proper
+          hydration practices, addressing both water quality and consumption. We
+          are currently designing and fabricating a smart water bottle that
+          leverages advanced sensor technology—down to the nanoscale—to monitor
+          both water quality and consumption.
+        </Text>
+        <Text>
+          From the city with &quot;water&quot; in its name, we&apos;re leading
+          the way in smarter, safer hydration.
+        </Text>
       </Stack>
-    </div>
+      <Stack className={classes.founders}>
+        <Title order={2}>Founders</Title>
+        <Image
+          radius="md"
+          alt="Team picture"
+          src="/assets/pictures/team-picture.jpg"
+        ></Image>
+      </Stack>
+      <Stack className={classes.story}>
+        <Title order={2}>Vivra&apos;s Story</Title>
+        <VivraTimeline active={1} items={vivraTimelineItems}></VivraTimeline>
+      </Stack>
+    </Stack>
   );
 }
--- vivra_bottle_app/src/app/page.tsx
+++ vivra_bottle_app/src/app/page.tsx
@@ -1,25 +1,51 @@
 "use client";
 import { ProgressRing } from "@/components/ProgressRing/ProgressRing";
-import { Badge, Group, Stack, Title } from "@mantine/core";
+import { Badge, Stack, Title } from "@mantine/core";
 
 import classes from "./page.module.css";
 import { WaterQualBadge } from "@/components/waterQualBadge/WaterQualBadge";
+import { SummaryCard } from "@/components/summaryCard/SummaryCard";
 
-export default function MyProfile() {
+// TODO Delete and Replace with name from database
+const name = "Mary";
+const consumptionGoal = 2.4;
+const amountDrank = 1.1;
+const percentageDrank = Math.round((amountDrank / consumptionGoal) * 100);
+
+export default function Summary() {
   return (
     <Stack className={classes.stack}>
-      <Title order={4}>Summary</Title>
-      <Title order={6}>Water Consumption</Title>
-      {/* TO DO work on this */}
-      <ProgressRing value={80} size={220} thickness={12}></ProgressRing>
-      <Group>
-        <Badge variant="light" color="blue">
-          Goal: 1.65 L/day
-        </Badge>
-        <Badge variant="light" color="rgba(39, 176, 167, 1)">
-          You drank: 1.1 L
-        </Badge>
-      </Group>
+      <Title order={2} className={classes.name}>
+        Welcome, {name}
+      </Title>
+      <SummaryCard
+        title="Water Consumption"
+        graph={
+          <ProgressRing
+            value={percentageDrank}
+            size={220}
+            thickness={12}
+          ></ProgressRing>
+        }
+        cardStats={
+          <Stack className={classes.consumptionStats}>
+            <Badge
+              variant="light"
+              color="blue"
+              className={classes.consumptionBadge}
+            >
+              Goal: {consumptionGoal} L/day
+            </Badge>
+            <Badge
+              variant="light"
+              color="rgba(39, 176, 167, 1)"
+              className={classes.consumptionBadge}
+            >
+              You drank: {amountDrank} L
+            </Badge>
+          </Stack>
+        }
+      ></SummaryCard>
       <Title order={6} mt={20}>
         Water Quality
       </Title>
