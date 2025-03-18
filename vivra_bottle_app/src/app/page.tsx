be4b9d330be7e8a45c91721a7fac2d80dc7b6e87
Mathew-Maradin <mathewm3800@gmail.com>
2025-03-17 21:25:40 -0400

    updating tool tip

--- vivra_bottle_app/src/app/page.tsx
+++ vivra_bottle_app/src/app/page.tsx
@@ -227,7 +227,7 @@ export default function Summary() {
       ></SummaryCard>
       <ConductivitySummaryCard
         data={conductivity}
-        tooltipLabel="Conductivity helps in analyzing factors that affect water quality like hardness and detect heavy metal presence to ensure a healthy balance of minerals in your water"
+        tooltipLabel="Conductivity informs mineral content dictating hydration quality to ensure a healthy balance of minerals in your water"
       ></ConductivitySummaryCard>
       <div style={{ marginTop: "50px" }}></div>
     </Stack>
